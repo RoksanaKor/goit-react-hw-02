385411fbd22d8f882539f587436638a3b7869d0a
RoksanaKor <roksanakorban@gmail.com>
2024-04-30 01:12:26 +0200

    first version phonebook step 1

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,43 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Phonebook.css";
-// import { nanoid } from "nanoid";
+import { nanoid } from "nanoid";
+
 
 export const Phonebook = () => {
- const [phonebook, setPhonebook] = useState({contacts: [], name: ''})
+  const [phonebook, setPhonebook] = useState([]);
 
- const handleSubmit = (e) => {
-  e.preventDefault();
-  const currentName = phonebook.name;
-  const currentState = {
-    ...phonebook,
-    contacts: [...phonebook.contacts, currentName]
+  const inputRef = useRef('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPhonebook([...phonebook, {name: inputRef.current.value , id: nanoid()}]);
+    inputRef.current.value = '';
   }
-  setPhonebook({...currentState})
-  console.log(phonebook);
- }
 
- return (
-  <>
-    <h2>Phonebook</h2>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name
-        <input
-         type="text"
-         name="name"
-         value={phonebook.name || ""}
-         pattern="^[a-zA-Zа-яА-Я]+((['\s\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-         required
-         onChange={(e) => setPhonebook({...phonebook, name: e.target.value})}
-        />
-      </label>
-      <button type="submit">Add contact</button>
-    </form>
-    <h2>Contacts</h2>
-    <ul>
-      {/* {phonebook.contacts.map((el) => <li key={el.id}>{el.name}</li>)} */}
-    </ul>
-  </>
- )
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+((['\s\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            ref={inputRef} 
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+      <h2>Contacts</h2>
+      <ul>
+        {phonebook.map((el) => <li key={el.id}>{el.name}</li>)}
+      </ul>
+    </>
+  )
 }

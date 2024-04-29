a548fe52cd6c22bff3022793913ace2cea9fe809
RoksanaKor <roksanakorban@gmail.com>
2024-04-29 23:13:45 +0200

    Marcin check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
 import { Feedback } from "./Feedback/Feedback.jsx";
+import { Phonebook } from "./Phonebook/Phonebook.jsx";
 import React, { useState } from "react";
 
 export const App = () => {
  const [stateValue, setStateValue] = useState({good: 0, neutral: 0, bad: 0});
+
 
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      <Feedback stateValue={stateValue} setStateValue={setStateValue}/>
+      {/* <Feedback stateValue={stateValue} setStateValue={setStateValue}/> */}
+      <Phonebook/>
     </div>
   );
 };
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Phonebook.css";
+import { nanoid } from "nanoid";
+
+export const Phonebook = () => {
+ const [phonebook, setPhonebook] = useState({contacts: [], name: ''})
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  const currentName = phonebook.name;
+  const currentState = {
+    ...phonebook,
+    contacts: [...phonebook.contacts, currentName]
+  }
+  setPhonebook({...currentState})
+  console.log(phonebook);
+ }
+
+ return (
+  <>
+    <h2>Phonebook</h2>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+         type="text"
+         name="name"
+         value={phonebook.name || ""}
+         pattern="^[a-zA-Zа-яА-Я]+((['\s\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+         required
+         onChange={(e) => setPhonebook({...phonebook, name: e.target.value})}
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+    <h2>Contacts</h2>
+    <ul>
+      {/* {phonebook.contacts.map((el) => <li key={el.id}>{el.name}</li>)} */}
+    </ul>
+  </>
+ )
+}
